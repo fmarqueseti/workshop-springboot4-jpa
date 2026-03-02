65c9639f5c8833d6f5bc548e563c4b45561e84ec
Fábio Marques <fmarques@fmarques.eti.br>
2026-03-02 12:26:02 -0300

    Service layer, component registration
    - adiciona injeção de dependência do UserService no UserResource
    - altera método findAll para retornar lista de usuários do serviço
    - remove instância mockada de usuário fixo
    - implementa endpoint GET /users/{id} para buscar usuário por id
    - adiciona arquivo de requisições do Postman

---
+++ src/main/java/br/eti/fmarques/course/services/UserService.java
@@ -0,0 +1,27 @@
+package br.eti.fmarques.course.services;
+
+import br.eti.fmarques.course.entities.User;
+import br.eti.fmarques.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+}
